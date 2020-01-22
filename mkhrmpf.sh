5b5ac259678e379c8125d87b3c33920ba4963f2a
David Stark <david@starkers.org>
2020-01-22 21:53:34 +0000

    download the build deps also

--- mkhrmpf.sh
+++ mkhrmpf.sh
@@ -12,6 +12,6 @@
 	-B extra/ipxe.iso \
 	-B extra/memtest86+-5.01.iso \
 	-B extra/grub2.iso \
-	-p "$(grep '^[^#].' hrmpf.packages)" \
+	-p "$(grep -hn '^[^#].' *.packages)" \
 	-A "gawk tnftp inetutils-hostname libressl-netcat dash vim-common" \
 	-U "zfs"
